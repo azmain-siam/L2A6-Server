5afbb0e5ed46b21f498db9a96a5170609b602a43
azmain-siam <siam.hossain.dev@gmail.com>
2025-03-17 05:46:02 +0600

    add: auth

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,5 @@
+import catchAsync from "../../utils/catchAsync";
+
+// const register = catchAsync(async(req:Request, res:Response)=>{
+// const result = await 
+// })
---
+++ src/modules/auth/auth.interface.ts
@@ -0,0 +1,5 @@
+export interface ILogin {
+  phone: string;
+  email: string;
+  password: string;
+}
