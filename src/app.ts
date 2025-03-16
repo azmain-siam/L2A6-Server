cef4dfa5f4786ecf668e60977ddd28ba307bdc95
azmain-siam <siamhossain2700@gmail.com>
2025-03-16 11:13:15 +0600

    catchAsync funtion

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,12 @@ import { StatusCodes } from "http-status-codes";
 
 const app: Application = express();
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173"],
-//     credentials: true,
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/user", userRouter);
 
@@ -20,10 +19,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Server is live⚡");
 });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  // console.log(err);
-
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ success: false, message: err.message, error: err });
+  res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ success: false, message: err.message, error: err });
 });
 
 export default app;
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,23 +1,20 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { UserService } from "./user.service";
 import { IUser } from "./user.interface";
 import sendResponse from "../../utils/sendResponse";
+import catchAsync from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const payload: IUser = req.body;
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const payload: IUser = req.body;
 
-    const result = await UserService.createUser(payload);
+  const result = await UserService.createUser(payload);
 
-    sendResponse(res, {
-      status: true,
-      message: "User created successfully",
-      data: result,
-    })
-  } catch (error) {
-    next(error);
-  }
-};
+  sendResponse(res, {
+    status: true,
+    message: "User created successfully",
+    data: result,
+  });
+});
 
 export const UserController = {
   createUser,
